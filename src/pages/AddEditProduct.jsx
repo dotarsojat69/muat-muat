4cc0479fcd2bd1d9dd31f62f3eb3e65c459f830e
dotarsojat69 <tdeath457@gmail.com>
2024-10-17 20:13:10 +0700

    feat: change to dialog/modal

--- src/pages/AddEditProduct.jsx
+++ src/pages/AddEditProduct.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { FaPlus } from 'react-icons/fa';
 import { addProduct } from '@/store/productSlice';
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 
 function AddEditProduk() {
   const dispatch = useDispatch();
@@ -34,65 +29,77 @@ function AddEditProduk() {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="name"
-          rules={{ required: 'Product name is required' }}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Product Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter product name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="price"
-          rules={{ 
-            required: 'Price is required',
-            pattern: {
-              value: /^\d+(\.\d{1,2})?$/,
-              message: 'Please enter a valid price'
-            }
-          }}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Price</FormLabel>
-              <FormControl>
-                <Input type="number" step="0.01" placeholder="Enter price" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="stock"
-          rules={{ 
-            required: 'Stock is required',
-            pattern: {
-              value: /^\d+$/,
-              message: 'Please enter a valid stock number'
-            }
-          }}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Stock</FormLabel>
-              <FormControl>
-                <Input type="number" placeholder="Enter stock" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Add Product</Button>
-      </form>
-    </Form>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="fixed bottom-4 right-4 rounded-full w-12 h-12 shadow-lg">
+          <FaPlus />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add New Product</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              rules={{ required: 'Product name is required' }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter product name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              rules={{ 
+                required: 'Price is required',
+                pattern: {
+                  value: /^\d+(\.\d{1,2})?$/,
+                  message: 'Please enter a valid price'
+                }
+              }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input type="number" step="0.01" placeholder="Enter price" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="stock"
+              rules={{ 
+                required: 'Stock is required',
+                pattern: {
+                  value: /^\d+$/,
+                  message: 'Please enter a valid stock number'
+                }
+              }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Stock</FormLabel>
+                  <FormControl>
+                    <Input type="number" placeholder="Enter stock" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Add Product</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
 
